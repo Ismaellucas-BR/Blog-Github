032bc61e0f10f894a2213c51f3f46096239216ba
Ismaellucas-BR <ismaellucas36@gmail.com>
2025-02-17 23:24:45 -0300

    Terminando filtro e compartilhando os dados da API pelos componentes

--- app/components/RepoList.tsx
+++ app/components/RepoList.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+interface Repo {
+  id: string;
+  name: string;
+  description?: string;
+  html_url: string;
+}
 
-function RepoList() {
-  return <div>RepoList</div>;
+interface RepoListProps {
+  repo: Repo;
+}
+
+function RepoList({ repo }: RepoListProps) {
+  return (
+    <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+      <div className="bg-post text-text rounded-lg pb-2.5 p-5">
+        <div>
+          <h2 className="font-bold text-xl font-nunito">{repo.name}</h2>
+          <span className="font-normal text-lg font-nunito">{`Há ${repo.}`}</span>
+        </div>
+
+        <p className="font-normal text-lg font-nunito">{repo.description}</p>
+      </div>
+    </a>
+  );
 }
 
 export default RepoList;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,7 @@ import ProfileContainer from "~/components/ProfileContainer";
 import type { Route } from "./+types/home";
 import RepoList from "~/components/RepoList";
 import { useEffect, useState } from "react";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -9,9 +10,17 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+interface Repo {
+  id: string;
+  name: string;
+  description?: string;
+  html_url: string;
+  public_repos: number;
+}
+
 export default function Home() {
-  const [profile, setProfile] = useState(null);
-  const [repos, setRepos] = useState([]);
+  const [profile, setProfile] = useState<Repo | null>(null);
+  const [repos, setRepos] = useState<Repo[]>([]);
   const [searchTerm, setsearchTerm] = useState("");
 
   useEffect(() => {
@@ -19,9 +28,9 @@ export default function Home() {
 
     fetch(`https://api.github.com/users/${username}`)
       .then((response) => response.json())
-      .then((data) => {
-        setProfile(data);
-        console.log(data);
+      .then((userProfile) => {
+        setProfile(userProfile);
+        console.log(userProfile);
       });
 
     fetch(`https://api.github.com/users/${username}/repos`)
@@ -32,17 +41,23 @@ export default function Home() {
       });
   }, []);
 
+  const filteredRepos = repos.filter((repo) => {
+    return repo.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <main className="flex flex-col items-center justify-center">
       <div className="w-[54rem]">
-        <ProfileContainer />
+        {profile && <ProfileContainer profile={profile} />}
         <section className="flex flex-col gap-2 mt-14">
           <div className="flex justify-between">
             <h2 className="text-subtitle font-nunito font-bold text-lg">
               Publicações
             </h2>
             <span className="text-span font-nunito font-normal text-sm">
-              6 publicações
+              {profile
+                ? `${profile.public_repos} publicações`
+                : "Carregando..."}
             </span>
           </div>
           <form className="w-full">
@@ -50,8 +65,14 @@ export default function Home() {
               className="bg-input text-text font-normal text-[1rem] font-nunito border border-border p-3 rounded-md w-full"
               placeholder="Buscar conteúdo"
               type="search"
+              onChange={(e) => setsearchTerm(e.target.value)}
             />
           </form>
+        </section>
+        <section className="flex flex-col gap-7">
+          {filteredRepos.map((repo) => (
+            <RepoList key={repo.id} repo={repo} />
+          ))}
         </section>
       </div>
     </main>
